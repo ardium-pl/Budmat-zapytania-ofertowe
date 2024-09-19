b371b5af96e3b5dea6866cd89ae793a08bb65e5a
Jakub <brodka.jakub@gmail.com>
2024-09-19 21:58:30 +0200

    image handling

--- src/attachments/fileHandler/imageHandler.js
+++ src/attachments/fileHandler/imageHandler.js
@@ -1,25 +1,26 @@
-const sharp = require('sharp');
-const logger = require('../../utils/logger');
-const { fileOcr } = require('./ocr.js');
+const sharp = require("sharp");
+const logger = require("../../utils/logger");
+const { fileOcr } = require("./ocr.js");
 
 async function processImage(filePath) {
-    try {
-        const metadata = await sharp(filePath).metadata();
-        const ocrData = await fileOcr(filePath);
+  try {
+    const metadata = await sharp(filePath).metadata();
+    const ocrData = await fileOcr(filePath);
 
-        // Convert ocrData to a string, assuming it's an object or array
-        const ocrDataString = Array.isArray(ocrData) 
-            ? ocrData.join('\n') 
-            : JSON.stringify(ocrData, null, 2);
+    let ocrContent;
+    // Extracting content from googleVisionText property
+    ocrContent = ocrData
+      .map((item) => item.googleVisionText || "")
+      .join("\n");
 
-        return `Image Metadata:\n${JSON.stringify(metadata, null, 2)}\n
-        Image OCR data:\n${ocrDataString}`;
-    } catch (error) {
-        logger.error('Error processing image:', error);
-        return `Error processing image: ${error.message}`;
-    }
+    return `Image Metadata:\n${JSON.stringify(metadata, null, 2)}\n
+Image OCR data:\n${ocrContent}`;
+  } catch (error) {
+    logger.error("Error processing image:", error);
+    return `Error processing image: ${error.message}`;
+  }
 }
 
 module.exports = {
-    processImage
+  processImage,
 };
