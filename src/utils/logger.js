66e01893e33f91b657e79bff555c5934a1c35292
Szymon Florek <szymon.florek@student.wat.edu.pl>
2024-09-23 18:07:51 +0200

    Refactor logger configuration to include log format

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,11 +1,15 @@
 const winston = require('winston');
 const path = require('path');
-const { format } = require('date-fns-tz');
+const { format, zonedTimeToUtc, utcToZonedTime } = require('date-fns-tz');
 
 // Konfiguracja formatu logów
 const logFormat = winston.format.combine(
     winston.format.timestamp({
-        format: () => format(new Date(), 'yyyy-MM-dd HH:mm:ss', { timeZone: 'Europe/Warsaw' })
+        format: () => {
+            const utcDate = zonedTimeToUtc(new Date(), 'Europe/Warsaw'); // Konwersja do UTC
+            const zonedDate = utcToZonedTime(utcDate, 'Europe/Warsaw'); // Konwersja do strefy czasowej Warsaw
+            return format(zonedDate, 'yyyy-MM-dd HH:mm:ss', { timeZone: 'Europe/Warsaw' });
+        }
     }),
     winston.format.errors({ stack: true }),
     winston.format.json()
@@ -19,19 +23,19 @@ const logger = winston.createLogger({
         // Zapisywanie wszystkich logów do pliku 'combined.log'
         new winston.transports.File({
             filename: path.join(__dirname, '../../logs/combined.log'),
-            format: winston.format.combine(logFormat, winston.format.json())
+            format: logFormat
         }),
         // Zapisywanie logów o poziomie 'error' i wyższym do pliku 'error.log'
         new winston.transports.File({
             filename: path.join(__dirname, '../../logs/error.log'),
             level: 'error',
-            format: winston.format.combine(logFormat, winston.format.json())
+            format: logFormat
         }),
         // Wyświetlanie logów w konsoli
         new winston.transports.Console({
             format: winston.format.combine(
                 winston.format.colorize(),
-                winston.format.simple()
+                logFormat
             )
         })
     ]
